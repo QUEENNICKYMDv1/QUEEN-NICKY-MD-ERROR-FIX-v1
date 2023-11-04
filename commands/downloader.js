e4a64a3f51ed28e7c9189431ac73a9669399335e
QUEENNICKYMDv1 <143050358+QUEENNICKYMDv1@users.noreply.github.com>
2023-11-04 18:47:16 +0530

    Update downloader.js

--- commands/downloader.js
+++ commands/downloader.js
@@ -59,43 +59,60 @@ catch(e){
 console.log(e)
 }})
     //---------------------------------------------------------------------------
-cmd({ 
-   pattern: 'apk', 
-   desc: 'Download APK', 
-   category: 'downloader', 
-   use:'<does this>', 
- }, async(Void,citel,text) => { 
- const args = text; 
- let search1 = await apks.search(args); 
- const id1 = search1[0].id ; 
- const apkname = search1[0].name ; 
- let apkdata = await apks.download(id1); 
- const dla = apkdata.dllink; 
- const icona = apkdata.icon; 
- const lastup = apkdata.lastup; 
- const size = apkdata.size;
-
- var rep = `* 📱QUEEN NICKY APK DOWNLOADER📱*
-
-*🔍 Name :* ${apkname}
-
-*📀 Package Name :* ${id1}
-
-*📲 Update On :* ${lastup}
-
-*📊 Size :* ${size}` ;
-
-await Void.sendMessage(citel.chat,{image:{url:icona,}, caption: rep,});
- return Void.sendMessage(citel.chat,{ 
-     document: { 
-         url: dla, 
-     }, 
-     fileName: apkname+'.apk', 
-     mimetype: "application/vnd.android.package-archive", 
- }, { 
-     quoted: citel, 
- }) 
-});
+const { tlang, botpic, cmd, prefix, runtime, Config, formatp, fetchJson } = require('../lib')
+const { download} = require('aptoide-scraper')
+cmd({
+    pattern: "apk",
+    alias: ["ps","downapk","playstore"],
+    desc: "download playstore app",
+    react: "🏷️",
+    category: "downloader",
+    filename: __filename,
+},
+async (Void, citel, text) => {
+if (!text) return
+try {
+let result = await download(text)
+ const applink = result.dllink
+    const getname = result.name
+    const icon = result.icon
+    const lastupdate = result.lastup
+    const packagename = result.package
+    const size = result.size
+      await Void.sendMessage(citel.chat, {
+        image: {
+            url: icon,
+        },
+        caption: `
+        \n ✧ *QUEEN NICKY APP DOWNLOADER*
+        \n━━━━━━━━━━━━━━━━━━
+        
+        \n ┇📚 *ᴀᴘᴘ ɴᴀᴍᴇ:* ${getname}
+        
+        \n ┇⬆️ *ʟᴀꜱᴛ ᴜᴘᴅᴀᴛᴇ:* ${lastupdate}
+        
+        \n ┇💻 *ᴘᴀᴄᴋᴀɢᴇ ɴᴀᴍᴇ:* ${packagename}
+        
+        \n ┇📊 *ꜰɪʟᴇ ꜱɪᴢᴇ:* ${size}
+        
+        \n ❭ *Qᴜᴇᴇɴ ɴɪᴄᴋʏ ᴍᴅ ° ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴄʏʙᴇʀ ᴍʀ-ᴅᴜᴍɪᴅᴜ*`,
+    })
+    return Void.sendMessage(citel.chat, {
+        document: {
+            url: applink,
+        },
+        mimetype: "application/vnd.android.package-archive",
+        fileName: getname,
+        caption: `🧚 *𝑄ᴜᴇᴇɴ ɴɪᴄᴋʏ ᴍᴅ ᴠ1*
+👩‍💻 *ᴄʀᴇᴛᴇᴅ ʙʏ ᴄʏʙᴇʀ ᴍʀ-ᴅᴜᴍɪᴅᴜ*`,
+    }, {
+        quoted: citel,
+    });
+  } catch (err) {
+    console.error(err);
+    citel.reply(` *❌ An error occurred while processing your request. Please try again later.* ${err}`);
+  }
+})
   //---------------------------------------------------------------------------
 cmd({
 
