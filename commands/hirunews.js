53a60830c80c30bea29f3c3eeb24f1361bb83e22
QUEENNICKYMDv1 <143050358+QUEENNICKYMDv1@users.noreply.github.com>
2023-11-05 17:23:12 +0530

    Update hirunews.js

--- commands/hirunews.js
+++ commands/hirunews.js
@@ -1,1 +1,28 @@
+const {cmd } = require('../lib')
+const hirunews = require('hirunews-scraper')
+cmd({
+        pattern: "hirunews",
+        category: "hiru",
+        react: "📑",
 
+        filename: __filename
+    },
+    async(Void, citel,text) => {
+    const hirunew = await hirunews()
+    const caption = `*_|📃 🌹⃝⃘𝐐𝐔𝐔𝐄𝐍 𝐍𝐈𝐂𝐊𝐘 𝐇𝐈𝐑𝐔𝐔 𝐍𝐄𝐖𝐒_* 
+
+🌹⃝⃘̉̉̉̉̉̉🧚 *ᴛɪᴛᴇʟ* :  ${hirunew.result.title}
+
+🌹⃝⃘̉̉̉̉̉̉🧚 *ᴅᴀᴛᴇ & ᴛɪᴍᴇ* :  ${hirunew.result.dateandtime}
+
+🌹⃝⃘̉̉̉̉̉̉🧚 *ᴜʀʟ* : ${hirunew.result.link}
+
+🌹⃝⃘̉̉̉̉̉̉🧚 *ᴅᴇꜱᴄʀɪᴘᴛɪᴏɴ* : ${hirunew.result.description}
+
+*┗━━━━━━━━━━━━━━⛶*
+
+ ©ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴍʀ ᴅᴜᴍɪᴅᴜ `
+
+await Void.sendMessage(citel.chat, { image: { url: hirunew.result.image }, caption: caption }, { quoted: citel })
+
+    })
