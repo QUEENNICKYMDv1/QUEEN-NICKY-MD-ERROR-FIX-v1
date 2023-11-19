4c9e06d1b079ca3dea6616de66575462d04cad51
QUEENNICKYMDv1 <143050358+QUEENNICKYMDv1@users.noreply.github.com>
2023-11-19 06:31:26 +0530

    Update Auto-voice-sender.js

--- commands/Auto-voice-sender.js
+++ commands/Auto-voice-sender.js
@@ -1,22 +1,25 @@
-if (icmd) { 
-             if (Config.readmessage==="true"){ 
-             await Void.readMessages([mek.key]) 
-             } 
-         } 
-         else { 
-             if (Config.voice==="true") { 
-                 switch(body){ 
-                     case 'Mk': case 'Hi': case 'Gm': case'Gn': case'sex': case'hukamu': case'I love you': case'Ado': case'අඩෝ': case'හ්මිමි': { 
-                         await Void.sendPresenceUpdate('recording', citel.chat); 
-                         const viourl = 'https://github.com/QUEENNICKYMDv1/QUEEN-NICKY-MD-AUTO-VOIC-SENDER/raw/main/'+body+'.mp3'; 
-                         await Void.sendMessage(citel.chat, { audio: {url : viourl,}, mimetype: 'audio/mpeg', ptt: true }, { quoted: citel, }); 
-                         await Void.readMessages([mek.key]) 
-                         } 
-                     break 
-                     default :{ 
-  
-                     } 
-                 } 
-  
-             } 
-          }
+const { tlang } = require('../lib')
+
+const Config = require('../config')
+
+const axios = require('axios')
+
+// Put here your url with modified words and urls
+
+const url = 'https://gist.githubusercontent.com/X-Notiya/cb99be100d09b54679b715d1763c6364/raw'//
+
+
+
+cmd({ on: "body" }, async (Void,citel,text)=> {
+
+ if (Config.autovoice === 'true' && citel.text.startsWith(prefix));
+
+  let { data } = await axios.get(url)
+
+  for (vr in data){
+
+ if((new RegExp(`\\b${vr}\\b`,'gi')).test(citel.text)) return Void.sendMessage(citel.chat,{audio: { url : data[vr]},mimetype: 'audio/mpeg',ptt:true},{quoted:citel})   
+
+}
+
+})
