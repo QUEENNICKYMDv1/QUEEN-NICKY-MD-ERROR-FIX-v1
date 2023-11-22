e19070529c2cd18b0322b542d12d2dc7e5caa91a
QUEENNICKYMDv1 <143050358+QUEENNICKYMDv1@users.noreply.github.com>
2023-11-22 20:34:02 +0530

    Update Auto-voice.js

--- commands/Auto-voice.js
+++ commands/Auto-voice.js
@@ -1,42 +1,16 @@
-const { tlang } = require('../lib')
+const { tlang,cmd } = require('../lib')
 
 const Config = require('../config')
 
 const axios = require('axios')
 
-// Put here your url with modified words and urls
 
-const url = 'const { tlang } = require('../lib')
 
-const Config = require('../config')
-
-const axios = require('axios')
-
-// Put here your url with modified words and urls
-
-const url = 'https://gist.github.com/QUEENNICKYMDv1/71fe553058fdb0130074d98ae2b2f057/raw'
+const url = 'https://gist.github.com/QUEENNICKYMDv1/942e096b2344d02bcb74e786b0008d50/raw'
 
 
 
-cmd({ on: "body" }, async (Void,citel,text)=> {
-
- if (Config.autovoice === 'true' && citel.text.startsWith(prefix));
-
-  let { data } = await axios.get(url)
-
-  for (vr in data){
-
- if((new RegExp(`\\b${vr}\\b`,'gi')).test(citel.text)) return Void.sendMessage(citel.chat,{audio: { url : data[vr]},mimetype: 'audio/mpeg',ptt:true},{quoted:citel})   
-
-}
-
-})'
-
-
-
-cmd({ on: "body" }, async (Void,citel,text)=> {
-
- if (Config.autovoice === 'true' && citel.text.startsWith(prefix));
+cmd({ on: "text" }, async (Void,citel,text,{isCreator})=> {
 
   let { data } = await axios.get(url)
 
